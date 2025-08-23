9ba798738157746d5e0a228c3b90e20ef42475e9
sudip-kandel <sudip-kandel7@gmail.com>
2025-08-23 21:20:44 +0545

    starting deployment

---
+++ navbar.php
@@ -0,0 +1,35 @@
+<nav class="flex justify-around h-[90px] border border-red-200 items-center text-[18px] font-bold">
+  <!-- Logo -->
+  <div class="flex items-center gap-5 text-[26px]">
+    <img src="./public/logo.png" class="w-16 h-16 rounded-full" alt="logo of the website">
+    <h1>Barber's Point</h1>
+  </div>
+
+  <!-- Menu -->
+  <div> 
+    <ul class="flex items-center gap-16 font-light text-[17px]">
+      <li class="cursor-pointer ">Home</li>
+      <li class="cursor-pointer">About Us</li>
+      <li class="cursor-pointer ">Contact</li>
+    </ul>
+  </div>
+
+  <!-- Auth Buttons -->
+  <div>
+    <ul class="flex items-center gap-5 font-light">
+      <li class="cursor-pointer text-[17px]">
+        <a href="./login.php" class="flex items-center gap-4  border-yellow-400 font-bold w-36 h-14 border-2 justify-center rounded-[7px]">
+            <img src="./public/enter.png" class="w-3 h-4" alt="login logo">
+            <p class="text-yellow-400">Login</p>
+
+</a>
+      </li>
+      <li class="cursor-pointer bg-black w-28 h-14 text-white flex items-center justify-center rounded-[7px] text-[17px]">
+        Register
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
