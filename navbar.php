72d8b1dfa34b378d7d72d4ac45b5f18b361b1e7a
sudip-kandel <sudip-kandel7@gmail.com>
2025-08-28 07:53:59 +0545

    

--- navbar.php
+++ navbar.php
@@ -1,35 +1,48 @@
-<nav class="flex justify-around h-[90px] border border-red-200 items-center text-[18px] font-bold">
-  <!-- Logo -->
-  <div class="flex items-center gap-5 text-[26px]">
+ <nav class="flex justify-around h-[90px] border border-red-200 items-center font-bold">
+  <div class="flex items-center gap-5 text-[27px]">
     <img src="./public/logo.png" class="w-16 h-16 rounded-full" alt="logo of the website">
     <h1>Barber's Point</h1>
   </div>
 
-  <!-- Menu -->
   <div> 
-    <ul class="flex items-center gap-16 font-light text-[17px]">
-      <li class="cursor-pointer ">Home</li>
-      <li class="cursor-pointer">About Us</li>
-      <li class="cursor-pointer ">Contact</li>
-    </ul>
+<ul class="flex items-center gap-11 font-light text-[23px]">
+  <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
+    Home
+    <hr class="w-0 border-0 h-[3px] bg-yellow-400 rounded-lg transition-all duration-300 group-hover:w-full">
+  </li>
+  <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
+    About Us
+    <hr class="w-0 border-0 h-[3px] bg-yellow-400 rounded-lg transition-all duration-300 group-hover:w-full">
+  </li>
+  <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
+    Contact
+    <hr class="w-0 border-0 h-[3px] bg-yellow-400 rounded-lg transition-all duration-300 group-hover:w-full">
+  </li>
+</ul>
+
+
+
+
+
   </div>
 
-  <!-- Auth Buttons -->
   <div>
     <ul class="flex items-center gap-5 font-light">
-      <li class="cursor-pointer text-[17px]">
-        <a href="./login.php" class="flex items-center gap-4  border-yellow-400 font-bold w-36 h-14 border-2 justify-center rounded-[7px]">
-            <img src="./public/enter.png" class="w-3 h-4" alt="login logo">
-            <p class="text-yellow-400">Login</p>
-
-</a>
-      </li>
-      <li class="cursor-pointer bg-black w-28 h-14 text-white flex items-center justify-center rounded-[7px] text-[17px]">
+ <li class="cursor-pointer text-[17px] ">
+  <a href="./login.php" class="flex items-center gap-4 border-yellow-500 ring-offset-background font-semibold w-36 h-14 border-2 justify-center rounded-[7px] text-yellow-600  shadow transition-all duration-300 hover:scale-105">
+  <img src="./public/enter.png" class="w-3 h-4" alt="login logo">
+  <p>Login</p>
+  </a>
+ </li>
+ <li class="cursor-pointer bg-black w-28 h-14 text-white flex items-center justify-center rounded-[7px] text-[17px] hover:bg-gray-900 shadow hover:shadow-lg transition-shadow duration-300">
         Register
       </li>
     </ul>
+  
+
   </div>
-</nav>
+</nav> 
+
 
 
 
