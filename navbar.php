334c03c964b95df618415ce580b2fe6cbef97ab7
sudip-kandel <sudip-kandel7@gmail.com>
2025-09-07 21:03:35 +0545

    logo

--- navbar.php
+++ navbar.php
@@ -1,58 +1,10 @@
-    <!-- <nav class="bg-white/95 backdrop-blur-md border-b border-gray-200 sticky top-0 z-50">
-  <div class="max-w-7xl mx-auto ">
-    <div class="flex items-center justify-between h-16">
-      
-      <div class="flex items-center gap-3">
-        <img src="./public/logo.png" class="w-10 h-10 rounded-full" alt="logo">
-        <span class="text-xl font-bold text-gray-900">Barber's Point</span>
-      </div>
-
-      <ul class="hidden md:flex items-center gap-10 font-light text-[18px]">
-        <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
-          Home
-          <hr class="w-0 border-0 h-[2px] bg-yellow-400 rounded-md transition-all duration-300 group-hover:w-full">
-        </li>
-        <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
-          About Us
-          <hr class="w-0 border-0 h-[2px] bg-yellow-400 rounded-md transition-all duration-300 group-hover:w-full">
-        </li>
-        <li class="group cursor-pointer transition-colors duration-300 hover:text-yellow-400">
-          Contact
-          <hr class="w-0 border-0 h-[2px] bg-yellow-400 rounded-md transition-all duration-300 group-hover:w-full">
-        </li>
-      </ul>
-
-      <div class="hidden md:flex items-center gap-4">
-        <a href="./login.php" 
-           class="flex items-center gap-2 border-2 border-yellow-500 font-semibold h-10 px-4 rounded-md text-yellow-600 shadow transition-all duration-300 hover:bg-yellow-500 hover:text-black hover:scale-105">
-          <img src="./public/enter.png" class="w-4 h-4" alt="login icon">
-          <span>Login</span>
-        </a>
-
-        <a href="./register.php"
-           class="bg-black text-white h-10 px-6 flex items-center justify-center rounded-md text-[15px] shadow hover:bg-gray-900 hover:shadow-lg transition-all duration-300">
-          Register
-        </a>
-      </div>
-
-       <button class="md:hidden p-2 rounded-md hover:bg-gray-100">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" 
-             viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                d="M4 6h16M4 12h16M4 18h16"/>
-        </svg>
-      </button> 
-    </div>
-  </div>
-</nav>  -->
-
-
 <nav class="bg-white/95 backdrop-blur-md border-b border-gray-200 sticky top-0 z-50">
   <div class="max-w-7xl mx-auto">
     <div class="flex items-center justify-between h-16">
       
-      <div class="flex items-center gap-3">
-        <img src="./public/logo.png" class="w-10 h-10 rounded-full" alt="logo">
+      <div class="flex items-center gap-1">
+        <!-- <img src="./public/ll.png" class="w-11 h-11 rounded-full" alt="logo"> -->
+         <img src="./public/logo.png" class="w-11 h-11 rounded-full bg-transparent" alt="logo">
         <span class="text-xl font-bold text-gray-900">Barber's Point</span>
       </div>
 
