588d365899588157ec653d51bd16370945fe7330
sudip-kandel <sudip-kandel7@gmail.com>
2025-09-07 20:12:07 +0545

    home

---
+++ home.php
@@ -0,0 +1,76 @@
+
+
+
+<section style="
+  background-image: url('./public/bg.jpeg');
+  background-size: cover;      
+  background-repeat: no-repeat;
+  background-position: center;
+" class="h-[82vh]">
+
+<div class="flex justify-center items-center flex-col text-center h-full">
+    <div class="">
+        <h1 class="text-6xl text-black font-bold">Find Your Perfect</h1>
+        <h1 class="text-6xl font-bold text-yellow-500">Barber Shop</h1>
+        <p class="text-xl text-gray-300 font-light mt-3">Discover professional barber shops near you. Book appointments,</p>
+        <p class="text-xl text-gray-300 font-light">check wait times, and read reviews from real customers.</p>
+       
+    </div>
+    
+    <div class="flex bg-white mt-9 mb-11 h-10 items-center border rounded-xl pl-3 px-2 gap-3 ">
+        <img src="./public/search-icon.png" class="w-7 h-7" alt="search icon">
+        <input type="search" class="h-full w-[499px] focus:outline-none" placeholder="Search for shops, locations...">
+        <div class="w-12 h-full flex items-center hover:bg-yellow-400 justify-center rounded-md border-gray-300 cursor-pointer">
+            <img src="./public/voice.png" class="w-6 h-6" alt="voice search icon">
+        </div>
+        <button class="bg-black text-white text-[25px] text-sm py-[8px] px-[24px] rounded-md hover:bg-gray-800" >Search</button>
+
+    </div>
+
+
+
+
+
+        
+
+      <div class="flex gap-8 md:gap-16 text-center text-gray-400">
+    <div class="  p-6    min-w-[180px]">
+        <div class="flex items-center justify-center gap-1 mb-3">
+            <p class="text-4xl font-bold text-yellow-400">50</p>
+            <img src="./public/cross.png" alt="" class="w-4 h-4">
+        </div>
+        <div class="flex items-center justify-center gap-2">
+            <img src="./public/shop.png" alt="" class="w-6 h-6">
+            <p class="text-gray-300 text-md">Barber Shops</p>
+        </div>
+    </div>
+
+    <div class=" p-6  min-w-[180px]">
+        <div class="flex items-center justify-center gap-1 mb-3">
+            <p class="text-4xl font-bold text-yellow-400">1000</p>
+            <span class="text-2xl text-yellow-400 font-bold">+</span>
+        </div>
+        <div class="flex items-center justify-center gap-2">
+            <img src="./public/heart.png" alt="" class="w-6 h-6">
+            <p class="text-gray-300 text-md">Happy Customers</p>
+        </div>
+    </div>
+
+    <div class="p-6 min-w-[180px]">
+        <div class="flex items-center justify-center gap-1 mb-3">
+            <p class="text-4xl font-bold text-yellow-400">4.8</p>
+            <img src="./public/star.png" alt="" class="w-5 h-5">
+        </div>
+        <div class="flex items-center justify-center gap-2">
+            <img src="./public/upward.png" alt="" class="w-6 h-6">
+            <p class="text-gray-300 text-md">Average Rating</p>
+        </div>
+    </div>
+</div>
+
+</div>
+
+</section>
+
+
+
